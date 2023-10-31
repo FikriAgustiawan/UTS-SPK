919dee306486ee0dfdbd053fbb42a4c48aa7ddc1
FikriAgustiawan <133733084+FikriAgustiawan@users.noreply.github.com>
2023-10-31 12:07:14 +0700

    Add files via upload

---
+++ alternatif_202310311137.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.alternatif ("Nama Gitar",merk,"Berat Gitar","Body Material","Scale Length","Type","Harga (Rp)",alternatif,"Nilai Akhir","Rank") VALUES
+	 ('Yamaha FG800','Yamaha','16 kg','Solid Spruce','635 mm','Acoustic',' Rp2,295,000.00 ',3.5208771,0.10685993,6),
+	 ('Martin LX1E','Martin','28 kg','Sitka Spruce','584 mm','Acoustic',' Rp8,799,000.00 ',3.5892677,0.10893561,5),
+	 ('Taylor GS Mini','Taylor','13 kg','Sitka Spruce','596 mm','Acoustic',' Rp16,000,000.00 ',3.319876,0.10075947,7),
+	 ('Fender Newporter','Fender','15 kg','Sitka Spruce','650 mm','Acoustic',' Rp7,000,000.00 ',3.8589702,0.11712118,3),
+	 ('Yamaha SLG200S','Yamaha','10 kg','Mahogany','634 mm','Silent Guitar',' Rp7,840,000.00 ',3.089182,0.09375782,8),
+	 ('Yamaha JR1','Yamaha','8 kg','Solid Spruce','580 mm','Classical Guitar',' Rp985,000.00 ',3.9005768,0.11838396,2),
+	 ('Fender CD60CE','Fender','7 kg','Solid Spruce','643 mm','Acoustic',' Rp5,800,000.00 ',4.0772533,0.12374615,1),
+	 ('Taylor Swift Baby Taylor','Taylor','20 kg','Sitka Spruce','577 mm','Acoustic',' Rp8,100,000.00 ',4.0137577,0.121819034,4),
+	 ('Ibanez Artcore AG95K-NT','Ibanez','22 kg','Acacia','628 mm','Elektrik Guitar',' Rp12,450,000.00 ',1.9055831,0.057835158,9),
+	 ('Ibanez GSR200B-WNF 4- String Bass','Ibanez','10 kg','Nyatoh Body','864 mm','Bass Guitar',' Rp2,745,000.00 ',1.6731822,0.050781704,10);
